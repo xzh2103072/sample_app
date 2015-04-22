5165f9fe438b1875f6c0801713687897e046b96d
xzh <xiazihang@cd.tuan800.com>
2015-04-22 14:43:11 +0800

    finish layout and routes

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe "UserPages" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+  subject { page }
+  describe "Signup page" do
+    before { visit signup_path }
+    it { should have_content('Sign up') }
+    it { should have_title("#{base_title} | Sign up")}
+  end
+end
