f9ca4670076c2b0e905ad5727fb18d4ad8ce357f
xzh <xiazihang@cd.tuan800.com>
2015-04-22 18:06:21 +0800

    validate complete

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,5 +20,62 @@ describe User do
     describe "when name is too long" do 
       before {@user.name = "a" * 51 }
       it {should_not be_valid}
-    end 
+    end
+   describe "when email format is invalid" do
+      it "should be invalid" do
+         addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+         addresses.each do |i|
+          @user.email = i 
+          expect(@user).not_to be_valid
+        end
+      end
+    end
+  
+   describe "when email format is valid" do
+      it "should be valid" do
+        addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+        addresses.each do |i|
+        @user.email = i
+        expect(@user).to be_valid
+        end
+      end
+    end
+  describe "when email address is already token" do
+    before do
+      user_with_same_email = @user.dup
+      user_with_same_email.email = @user.email.upcase
+      user_with_same_email.save
+    end
+   it {should_not be_valid}
+  end
+ 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
