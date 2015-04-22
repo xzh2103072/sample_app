eb10a36cc73ab64e00c4b96304d8958e71b0e16a
xzh <xiazihang@cd.tuan800.com>
2015-04-22 14:00:12 +0800

    layouts-filled

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,43 +4,41 @@ describe "StaticPages" do
    let(:base_title) { "Ruby on Rails Tutorial Sample App" }
   
    describe "Home page" do
+     before { visit root_path } 
     it "should have the content 'Sample App'" do
-      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
     it "should have the right title" do 
-      visit '/static_pages/home'
       expect(page).to have_title("#{base_title} | Home")
     end 
  end
   describe "Help page" do
     it "should have the content 'Help'" do
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_content('Help')
     end
     it "should have the right title" do 
-      visit '/static_pages/help'
+      visit help_path
       expect(page).to have_title("#{base_title} | Help")
     end
   end
   describe "About page" do
     it "should have the content 'About us'" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_content('About us')
     end
     it "should have the right title" do
-      visit '/static_pages/about'
+      visit about_path
       expect(page).to have_title("#{base_title} | About")
     end 
  end
   describe "Contact page" do
     it "should have the content 'Contact'" do 
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact')
     end
     it "should have the right title" do 
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_title("#{base_title} | Contact")
     end
   end
